d22d9d15017bff273aa54b7c714cfd75924a47ee
LuvWubs <iydkwtmiwt@gmail.com>
2018-07-09 14:39:39 -0600

    react refactor unsuccessful

---
+++ client/src/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import Nav from "./components/Nav";
+import Wrapper from "./components/Wra";
+import Presets from "./components/Presets";
+import Tables from "./components/Tables";
+import presets from "./seeds.json";
+import ReactHowler from 'react-howler';
+
+
+//NOTE check functionality w/o axios
+import axios from 'axios';
+
+//NOTE should this extend component or should it be incorporated into the code within App = () == =>
+class App extends Component {
+
+  state = {
+    presets
+  };
+
+  newSong = selector => {
+    event.preventDefault();
+    let song = this.state.presets.filter(song => presets.name !== selector);
+    this.setState({ song });
+  };
+
+
+//NOTE need to incorporate this onClick functionality onto DOM elements.. not sure what file to put this into????
+      $('#nav-expander').on('click',function(e){
+    		e.preventDefault();
+    		$('body').toggleClass('nav-expanded');
+    	});
+
+    	$('#nav-close').on('click',function(e){
+    		e.preventDefault();
+    		$('body').removeClass('nav-expanded');
+    	});
+      $('.list-soundThemes').on('click',function(e){
+    		e.preventDefault();
+    		$('body').removeClass('nav-expanded');
+    	});
+
+
+const App = () =>
+
+// render() {
+//   return (
+    <Nav />
+    <Wrapper>
+      {this.state.presets.map(preset => (
+        <Presets
+          key={presets.name}
+          name={presets.name}
+          file={presets.file}
+          description={presets.description}
+          newSong={this.newSong}
+        />
+      ))}
+    </Wrapper>
+//   );
+  // }
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const Spotify = require('node-spotify-api');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+
+//NOTE use spotify or dump this code
+
+// app.get('/api/search', (req, res) => {
+//
+//   // /api/search?q=Griz
+//   const query = req.params.q;
+//
+//   console.log('req.params', req.params);
+//
+//   var spotify = new Spotify({
+//     // TODO: put these somewhere safe
+//     id: '824fe7c92f5d4a48a4f0adb39cdfbf10',
+//     secret: '6d0b2a4874844f1c88fe185bcbd95781'
+//   });
+//
+//   spotify.search({ type: 'track', query: this.state.query }, function(err, data) {
+//     if (err) {
+//       return console.log('Error occurred: ' + err);
+//     }
+//
+//     res.json(data);
+//   });
+//
+// });
+
+app.listen(port, () => console.log('Server running on port', port));
